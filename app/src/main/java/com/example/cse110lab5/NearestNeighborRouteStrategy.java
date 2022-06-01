304e2bb0f96d45b4d87ccefd450a82a80e42d786
Ilan Cosman <ilancosman@gmail.com>
2022-05-31 23:52:23 -0700

    Rest of the changes

--- app/src/main/java/com/example/cse110lab5/NearestNeighborRouteStrategy.java
+++ app/src/main/java/com/example/cse110lab5/NearestNeighborRouteStrategy.java
@@ -1,38 +1,47 @@
 package com.example.cse110lab5;
 
+import android.util.Log;
+
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class NearestNeighborRouteStrategy implements RouteStrategy {
     @Override
-    public List<String> makeRoute(Graph<String, IdentifiedWeightedEdge> edgeData, List<String> selectedExhibits) {
-        return makeRouteRecursively(edgeData, selectedExhibits, "entrance_exit_gate");
+    public List<String> makeRoute(Graph<String, IdentifiedWeightedEdge> edgeData, Map<String, ZooData.VertexInfo> vertexData, List<String> selectedExhibits) {
+        return makeRouteRecursively(edgeData, vertexData, selectedExhibits, "entrance_exit_gate");
     }
 
-    private List<String> makeRouteRecursively(Graph<String, IdentifiedWeightedEdge> edgeData, List<String> remainingExhibits, String current) {
+    private List<String> makeRouteRecursively(Graph<String, IdentifiedWeightedEdge> edgeData, Map<String, ZooData.VertexInfo> vertexData, List<String> remainingExhibits, String current) {
         List<String> output = new ArrayList<>();
         output.add(current);
 
         remainingExhibits.remove(current);
 
         if (!remainingExhibits.isEmpty()) {
-            String closest = getClosest(edgeData, remainingExhibits, current);
-            output.addAll(makeRouteRecursively(edgeData, remainingExhibits, closest));
+            String closest = getClosest(edgeData, vertexData, remainingExhibits, current);
+            output.addAll(makeRouteRecursively(edgeData, vertexData, remainingExhibits, closest));
         }
 
         return output;
     }
 
-    private String getClosest(Graph<String, IdentifiedWeightedEdge> edgeData, List<String> remainingExhibits, String current) {
+    private String getClosest(Graph<String, IdentifiedWeightedEdge> edgeData, Map<String, ZooData.VertexInfo> vertexData, List<String> remainingExhibits, String current) {
+        current = getGroupIdIfNecessary(vertexData, current);
+
+        Log.e("getClosest", String.format("%s", current));
+
         double minDistance = Double.MAX_VALUE;
         String closest = "";
 
         for (String exhibit : remainingExhibits) {
-            GraphPath<String, IdentifiedWeightedEdge> path = DijkstraShortestPath.findPathBetween(edgeData, current, exhibit);
+            String groupIdedExhibitIfNecessary = getGroupIdIfNecessary(vertexData, exhibit);
+
+            GraphPath<String, IdentifiedWeightedEdge> path = DijkstraShortestPath.findPathBetween(edgeData, current, groupIdedExhibitIfNecessary);
             double pathLength = path.getWeight();
 
             if (pathLength < minDistance) {
@@ -42,5 +51,13 @@ public class NearestNeighborRouteStrategy implements RouteStrategy {
         }
 
         return closest;
+    }
+
+    private String getGroupIdIfNecessary(Map<String, ZooData.VertexInfo> vertexData, String vertex) {
+        ZooData.VertexInfo currentVertex = vertexData.get(vertex);
+        if (currentVertex.getInGroup()) {
+            return currentVertex.getGroupId();
+        }
+        return vertex;
     }
 }
